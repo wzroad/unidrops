8245af275f25dae0b349bc90c5a87cb85c39d35b
wzroad <wzroad1994@gmail.com>
2024-11-14 21:10:35 +0800

    feat 优化环境变量

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const mailTest = {
+  from: '"空投学院" <web3@guooni.com>',
+  host: "smtp.qiye.aliyun.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "web3@guooni.com",
+    pass: "sdykJYM1994",
+  },
+};
+const mailProd = {
+  from: '"空投学院" <info@onechainhub.com>',
+  host: "smtp-mail.outlook.com",
+  port: 587,
+  secure: true,
+  auth: {
+    user: "info@onechainhub.com",
+    pass: "ABCDEF9*P",
+  },
+};
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
@@ -26,26 +47,7 @@ export default defineNuxtConfig({
     installStudio: false,
     generateClient: process.env.NODE_ENV === "production",
   },
-  nodemailer: {
-    from: '"空投学院" <web3@guooni.com>',
-    host: "smtp.qiye.aliyun.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "web3@guooni.com",
-      pass: "sdykJYM1994",
-    },
-  },
-  // nodemailer: {
-  //   from: '"空投学院" <info@onechainhub.com>',
-  //   host: "smtp-mail.outlook.com",
-  //   port: 587,
-  //   secure: true,
-  //   auth: {
-  //     user: "info@onechainhub.com",
-  //     pass: "ABCDEF9*P",
-  //   },
-  // },
+  nodemailer: process.env.NODE_ENV === "production" ? mailProd : mailTest,
   auth: {
     // 这里添加你的 nuxt-auth 配置
     baseURL: "/api/auth",
